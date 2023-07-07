7bf0b4f67fab5c17156823539c91d8f5e6695db9
VInicius Schuelter <48526428+viniciusschuelter@users.noreply.github.com>
2023-07-07 15:32:25 -0300

    feat(pelada): phone login with firebase in progress

--- app.config.js
+++ app.config.js
@@ -1,16 +1,16 @@
 export default {
     expo: {
         "name": "jogo-pegado-rn",
-            "slug": "jogo-pegado-rn",
-            "version": "1.0.0",
-            "orientation": "portrait",
-            "icon": "./assets/images/icon.png",
-            "scheme": "myapp",
-            "userInterfaceStyle": "automatic",
-            "splash": {
-            "image": "./assets/images/splash.png",
-                "resizeMode": "contain",
-                "backgroundColor": "#ffffff"
+        "slug": "jogo-pegado-rn",
+        "version": "1.0.0",
+        "orientation": "portrait",
+        "icon": "./assets/images/icon.png",
+        "scheme": "myapp",
+        "userInterfaceStyle": "automatic",
+        "splash": {
+        "image": "./assets/images/splash.png",
+        "resizeMode": "contain",
+        "backgroundColor": "#ffffff"
         },
         "assetBundlePatterns": [
             "**/*"
@@ -22,23 +22,18 @@ export default {
             "adaptiveIcon": {
                 "foregroundImage": "./assets/images/adaptive-icon.png",
                     "backgroundColor": "#ffffff"
-            }
+            },
+            "package": "com.vinilona182.jogo_pegado_rn",
+            "versionCode": 1
         },
         "web": {
             "bundler": "metro",
                 "favicon": "./assets/images/favicon.png"
-        }
-    },
-    extra: {
-        eas: {
-            projectId: "45e35ba2-bc54-49f9-8aba-3942561f820e"
         },
-        firebaseApiKey: "AIzaSyCxhC_xA5_qFqBeBF_54J-womfsLaT0HZg",
-        firebaseAuthDomain: "jogo-pegado.firebaseapp.com",
-        firebaseProjectId: "jogo-pegado",
-        firebaseStorageBucket: "jogo-pegado.appspot.com",
-        firebaseMessagingSenderId: "987130126184",
-        firebaseAppId: "1:987130126184:web:b451d83f149563205840dd",
-        firebaseMeasurementId: "G-DZP52W167G"
+        "extra": {
+            "eas": {
+                "projectId": "45e35ba2-bc54-49f9-8aba-3942561f820e"
+            }
+        }
     }
 }
--- config/firebase.js
+++ config/firebase.js
@@ -1,12 +1,19 @@
-import config from '../app.config';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: config.extra.firebaseApiKey,
-    authDomain: config.extra.firebaseAuthDomain,
-    projectId: config.extra.firebaseProjectId,
-    storageBucket: config.extra.firebaseStorageBucket,
-    messagingSenderId: config.extra.firebaseMessagingSenderId,
-    appId: config.extra.firebaseAppId
+    apiKey: "AIzaSyCxhC_xA5_qFqBeBF_54J-womfsLaT0HZg",
+    authDomain: "jogo-pegado.firebaseapp.com",
+    projectId: "jogo-pegado",
+    storageBucket: "jogo-pegado.appspot.com",
+    messagingSenderId: "987130126184",
+    appId: "1:987130126184:web:b451d83f149563205840dd",
+    measurementId: "G-DZP52W167G"
 };
 
-module.exports = firebaseConfig;
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
